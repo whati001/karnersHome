4ff0e0f7468caba9589b42cf1d6be9902c80add2
whati001 <andreas.karner@student.tugraz.at>
2020-11-14 11:38:24 +0100

    AK - added homepage skeleton

---
+++ homepage/src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * @file main.js
+ * @description main js entry point
+ * @author andreas.karner@student.tugraz.at
+ */
+
+/** 
+ * Initialize qManager and load selected action
+ */
+window.onload = function() {
+  console.log('INFO: Please add some js code if needed.');
+};
